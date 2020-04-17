c98d99a21ffb34d1dd0585a0645460dda558fee3
Matheus <matgda@gmail.com>
2020-04-16 21:30:40 -0300

    Refatoração código duplicados

--- pages/Buscar/script.js
+++ pages/Buscar/script.js
@@ -1,8 +1,24 @@
 
 
-const url = "http://localhost:8080/api/alunos"
+ var url
 const urlRemove = "http://localhost:8080/api/aluno/"
 var listaAlunos
+var nome = document.getElementById("campoBusca").value
+
+
+function verificarLista( ){
+
+  var nome = document.getElementById("campoBusca").value
+   if(nome == ""){
+      console.log("Valor nulo")
+      url ="http://localhost:8080/api/alunos"
+   }else{
+      url = "http://localhost:8080/api/alunos"+"/"+nome
+   }
+   
+}
+
+
 
 function remove(idAluno) {
    fetch(urlRemove + idAluno, { method: 'DELETE' }).then(() => {
@@ -14,6 +30,8 @@ function remove(idAluno) {
 }
 
 function getAluno() {
+   verificarLista();
+
    fetch(url)
       .then(response => response.json())
       .then(data => {
@@ -45,41 +63,7 @@ function getAluno() {
    `
    }
 }
-function getAlunoNome() {
-   var nome = document.getElementById("campoBusca").value
 
-   fetch(url + "/" + nome)
-      .then(response => response.json())
-      .then(data => {
-         listaAlunos = data
-         document.getElementById('teste').innerHTML = `
-    ${data.map(alunoTemplate).join(' ')}
-
- `
-         document.getElementById('titulo').innerHTML = `
- <h1 class="title is-2" style="text-align: center">Lista de Alunos (${data.length})</h1>
- <br>
-
- `})
-      .catch(err => console.log(err))
-
-   function alunoTemplate(aluno) {
-      return `		
-      <div class="box">
-      <strong>Nome:</strong> ${aluno.nome}<br>
-   <strong>Idade:</strong> ${aluno.idade}<br>
-   <strong>Sexo:</strong> ${aluno.sexo}<br>
-   <img src="data:image/png;base64,${aluno.foto}"/><br>
-    <footer class="card-footer">
-    <a href="../alterarAluno/index.html" class="card-footer-item" onclick="atualizarAluno(${aluno.id})"  >Editar</a>
-    <a href="#" class="card-footer-item"  onclick="remove(${aluno.id})">Deletar</a>
-   
-  </footer>
-      </div>
-   `
-   }
-
-}
  function atualizarAluno(aluno) {
    
    var idAluno = aluno
@@ -116,17 +100,6 @@ console.log(atualizarAluno)
  
    
 
-/*  
-var n = "Joao"
-    document.getElementById('nome_crianca').value = n
-
-   document.getElementById('sexo_crianca').value = campoSexo
-
-   document.getElementById('idade_crianca').value = campoIdade
-
-   document.getElementById('carteira_identidade').value = campoCI
- window.open("editarAluno.html", "Altualizar Dados do Aluno", "height=600,width=600");
-   */
 
   
 }
@@ -162,11 +135,3 @@ function postCrianca() {
 
 }
 
-function pesquisar(event){
-
-   if (event.which == 13 || event.keyCode == 13) {
-      
-      getAlunoNome()
-  }
-  return true;
-}
