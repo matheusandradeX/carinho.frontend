a985ce472eff50004b610eeb38d895ffec5cae87
DESKTOP-QVEB3BV\Matheus <matheusandrade193@gmail.com>
2021-05-30 14:07:47 -0300

    Logo Adicionado

--- pages/Login/script.js
+++ pages/Login/script.js
@@ -1,14 +1,9 @@
+function myFunction() {
 
 
-function autenticar(){
-
-    console.log("teste");
-    var usuario = document.getElementById("usuario").value
-
-    var senha = document.getElementById("senha").value
-
-    console.log(usuario);
-    console.log(senha);
-
-}
-
+    if(document.getElementById("name").value==="mat" && document.getElementById("pass").value==="mat"){
+        window.location.href = "../Buscar/index.html";
+    }
+    
+    }
+ 
